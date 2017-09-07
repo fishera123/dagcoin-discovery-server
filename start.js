575be80b6d71bf6a0419ba4886779fa78576bc65
Yary Ribero <yary.ribero@ignite.ee>
2017-09-07 14:04:11 +0300

    The discovery service is able to process keep alive messages

--- start.js
+++ start.js
@@ -1,24 +1,63 @@
 /*jslint node: true */
 "use strict";
-var constants = require('byteballcore/constants.js');
-var conf = require('byteballcore/conf.js');
-var db = require('byteballcore/db.js');
 var eventBus = require('byteballcore/event_bus.js');
 var headlessWallet = require('headless-byteball');
-var ValidationUtils = require("byteballcore/validation_utils.js");
 
-eventBus.on('paired', function(deviceAddress){
-	if (headlessWallet.isControlAddress(deviceAddress)){
-		headlessWallet.handlePairing(deviceAddress);
-	}	
+eventBus.on('paired', function (deviceAddress) {
+    if (headlessWallet.isControlAddress(deviceAddress)) {
+        headlessWallet.handlePairing(deviceAddress);
+    }
 });
 
-eventBus.on('text', function(deviceAddress, text){
+eventBus.on('text', function (deviceAddress, text) {
+    processAsDagcoinMessage(deviceAddress, text);
+});
+
+// One device can send such message to check whether another device can exchange messages
+eventBus.on('dagcoin.is-connected', (message, fromAddress) => {
+    const reply = {
+        protocol: 'dagcoin',
+        title: 'connected'
+    };
+
+    const device = require('byteballcore/device.js');
+    device.sendMessageToDevice(fromAddress, 'text', JSON.stringify(reply));
+});
+
+/**
+ * Process a message considering the possibility it is a Dagcoin message.
+ * It tries to parse it, if it succeeds and if a protocol property is present (and set to dagcoin) then it
+ * emits the appropriate event.
+ * @param deviceAddress The message sender address
+ * @param body The message body as pure text
+ * @returns {Promise}
+ */
+function processAsDagcoinMessage(deviceAddress, body) {
+    let message = null;
+
+    try {
+        message = JSON.parse(body);
+    } catch (err) {
+        console.log(`NEW MESSAGE FROM ${deviceAddress}: ${body} NOT A JSON MESSAGE: ${err}`);
+    }
+
+    if (message === null) {
+        console.log(`NEW MESSAGE FROM ${deviceAddress}: EMPTY`);
+        return;
+    }
+
+    if (message.protocol === 'dagcoin') {
+        console.log(`DAGCOIN MESSAGE RECEIVED FROM ${deviceAddress}`);
+        eventBus.emit(`dagcoin.${message.title}`, message, deviceAddress);
+        return;
+    }
+
+    console.log(`JSON MESSAGE RECEIVED FROM ${deviceAddress} WITH UNEXPECTED PROTOCOL: ${message.protocol}`);
     var device = require('byteballcore/device.js');
     var db = require('byteballcore/db.js');
     var ds = require('./discovery-service.js');
-	var discoveryService = new ds.DiscoveryService(device, db);
+    var discoveryService = new ds.DiscoveryService(device, db);
     discoveryService.processCommand(deviceAddress, text);
-});
+}
 
 module.exports = headlessWallet;
