63305e9f6d382ad02f88e62fa46977fd5c7413bf
Yary Ribero <yary.ribero@ignite.ee>
2017-09-11 15:25:07 +0300

    Fixed a dangerous typo in the configuration file

--- conf.js
+++ conf.js
@@ -10,7 +10,7 @@ exports.storage = 'sqlite';
 
 exports.hub = 'byteball.org/bb-test';
 exports.deviceName = 'Headless';
-exports.permanent_paring_secret = '0000';
+exports.permanent_pairing_secret = '0000';
 exports.control_addresses = ['BX53JJ55D6SSVKTH2H44THOZWM4IGTZS'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 
